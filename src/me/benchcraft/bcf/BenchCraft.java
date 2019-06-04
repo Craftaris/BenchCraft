c930991d9d8b619f17d09697afa1950e83b3401b
Craftaris <daniel.warszawik@gmail.com>
2019-06-04 20:41:23 +0100

    Create BenchCraft.java

---
+++ src/me/benchcraft/bcf/BenchCraft.java
@@ -0,0 +1,71 @@
+package me.benchcraft.bcf;
+
+import java.io.File;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import me.benchcraft.bcf.listeners.ListenerClass;
+
+public class BenchCraft extends JavaPlugin
+{
+private File databaseFile;
+private File configFile;
+private FileConfiguration database;
+private FileConfiguration config;
+
+	/**
+	 * Uruchomienie pluginu.
+	 */
+
+	public void onEnable() {
+		
+		new ListenerClass(this);
+		registerDatabaseFiles();
+		registerCommands();
+		registerConfig();
+		getConfig().options().copyDefaults(true);
+		
+	}
+	
+	/**
+	 * Rejestracja komend - na razie brak.
+	 */
+
+	public void registerCommands() {
+
+	}
+
+	/**
+	 * Rejestracja bazy danych - na razie z niej nie korzystamy.
+	 */
+	
+	private void registerDatabaseFiles() {
+		this.databaseFile = new File(getDataFolder(), "database.yml");
+		this.database = YamlConfiguration.loadConfiguration(this.databaseFile);
+		saveDatabase();
+	}
+
+	public void saveDatabase() {
+		try {
+			this.database.save(this.databaseFile);
+		}
+		catch (Exception localException) {
+		}
+	}
+   
+	public File getDatabaseFile() {
+		return this.databaseFile;
+	}
+ 
+	public FileConfiguration getDatabase() {
+		return this.database;
+	}
+   
+	private void registerConfig() {
+	    this.configFile = new File(getDataFolder(), "config.yml");
+	    this.config = YamlConfiguration.loadConfiguration(this.configFile);
+	    getConfig().options().copyDefaults(true);
+		saveConfig();
+	}
+ }
